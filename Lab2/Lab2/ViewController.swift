e19c79142e0f6d8291055315c695e7ce54d16a99
Austin Griffith <augr4882@colorado.edu>
2018-09-20 00:00:26 -0600

    working copies

--- Lab2/Lab2/ViewController.swift
+++ Lab2/Lab2/ViewController.swift
@@ -10,6 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var titleBanner: UILabel!
+    @IBOutlet weak var dogImage: UIImageView!
+    @IBOutlet weak var imageSwitch: UISegmentedControl!
+    @IBOutlet weak var capitalSwitch: UISwitch!
+    @IBOutlet weak var fontSizeBanner: UILabel!
+    @IBOutlet weak var fontSlider: UISlider!
+    
+    
+    func changeImage(){
+        if imageSwitch.selectedSegmentIndex == 0 {
+            titleBanner.text="Thats a good dog"
+            dogImage.image=UIImage(named: "goodDog")
+        }
+        else if imageSwitch.selectedSegmentIndex == 1 {
+            titleBanner.text="Not so good dog"
+            dogImage.image=UIImage(named: "badDog")
+        }
+    }
+    
+    func makeCapital(){
+        if capitalSwitch.isOn {
+            titleBanner.text=titleBanner.text?.uppercased()
+        } else {
+            titleBanner.text=titleBanner.text?.lowercased()
+        }
+    }
+    
+    @IBAction func changeInfo(_ sender: UISegmentedControl) {
+        changeImage()
+        makeCapital()
+    }
+    
+    @IBAction func updateCaps(_ sender: UISwitch) {
+        makeCapital()
+    }
+    
+    
+    @IBAction func changeFontSize(_ sender: UISlider) {
+        let fontSize=sender.value   //this has to be a float value
+        
+        fontSizeBanner.text = String(format: "%.0f", fontSize) //convert float to String
+        
+        let fontSizeCGFloat=CGFloat(fontSize) //convert float to CGFloat
+        
+        titleBanner.font = UIFont.systemFont(ofSize: fontSizeCGFloat) //create a UIFont object and assign to the font property
+
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
