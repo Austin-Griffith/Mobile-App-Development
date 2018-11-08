a8a2a9001970ded8ab3d5c79cb5fe93662e36b83
Austin Griffith <augr4882@colorado.edu>
2018-11-07 22:11:55 -0700

    inital commit

---
+++ Lab52/app/src/main/java/com/example/austingriffith/lab_5/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.austingriffith.lab_5;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayBoo(View view)
+    {
+        EditText name = findViewById(R.id.editText);
+        String nameValue = name.getText().toString();
+        TextView booText = findViewById(R.id.message);
+        booText.setText("Happy Halloween " + nameValue + "!");
+        ImageView mlb = findViewById(R.id.imageView);
+        mlb.setImageResource(R.drawable.mlb);
+
+    }
+}
